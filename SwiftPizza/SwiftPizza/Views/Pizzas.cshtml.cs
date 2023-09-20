b8c231e3925f95b683c88657afc5a22b5f616fbf
Parth Talwar <parthtalwar@hotmail.com>
2023-09-20 16:07:13 +1200

    Created Pizzas.cshtml and created a tab for it on the index page. (WIP)

---
+++ SwiftPizza/SwiftPizza/Views/Pizzas.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SwiftPizza.Views
+{
+    public class PizzasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
