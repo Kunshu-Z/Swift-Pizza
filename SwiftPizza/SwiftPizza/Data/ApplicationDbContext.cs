a4626609b88021bc11d23f8302c15273a51e255b
Jordan Vera <Jordan.Vera01@student.weltec.ac.nz>
2023-09-25 10:59:45 +1300

    Before Migration backup

---
+++ SwiftPizza/SwiftPizza/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SwiftPizza.Models;
+
+namespace SwiftPizza.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        // Define DbSet properties for your tables
+        public DbSet<User> Users { get; set; }
+        public DbSet<Pizza> Pizzas { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Bank> Banks { get; set; }
+    }
+}
