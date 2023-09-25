a09940d5b3bda86f26aad27e1c506b7b5ab49241
Jordan Vera <Jordan.Vera01@student.weltec.ac.nz>
2023-09-25 13:44:12 +1300

    Migration1

--- SwiftPizza/SwiftPizza/Models/Bank.cs
+++ SwiftPizza/SwiftPizza/Models/Bank.cs
@@ -4,9 +4,10 @@ namespace SwiftPizza.Models
 {
     public class Bank
     {
+        [Key]
         public int CardNumber { get; set; }
-        
-        public string Address { get; set; }
+        [Required]
+        public string Name { get; set; }
 
         public DateTime Expiry { get; set; }
 
--- SwiftPizza/SwiftPizza/Models/Cart.cs
+++ SwiftPizza/SwiftPizza/Models/Cart.cs
@@ -5,6 +5,10 @@ namespace SwiftPizza.Models
 {
     public class Cart
     {
+        [Key]
+        public int CartId { get; set; } 
+
+        [Required]
         public int Price { get; set; }
     }
 }
