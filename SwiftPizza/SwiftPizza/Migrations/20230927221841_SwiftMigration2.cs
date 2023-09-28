9491fb0e9aba74fa38599e4d2434102b66d9034c
Jordan Vera <Jordan.Vera01@student.weltec.ac.nz>
2023-09-28 14:40:50 +1300

    A

---
+++ SwiftPizza/SwiftPizza/Migrations/20230927221841_SwiftMigration2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SwiftPizza.Migrations
+{
+    /// <inheritdoc />
+    public partial class SwiftMigration2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
