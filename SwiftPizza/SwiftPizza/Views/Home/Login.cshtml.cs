c247ddbe3e295a9a5fbc322ce1ca23ac87c5e2ee
Jordan Vera <Jordan.Vera01@student.weltec.ac.nz>
2023-09-28 14:43:51 +1300

    Change rollback

--- SwiftPizza/SwiftPizza/Views/Home/Login.cshtml.cs
+++ SwiftPizza/SwiftPizza/Views/Home/Login.cshtml.cs
@@ -1,50 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-using SwiftPizza.Data;
-using SwiftPizza.Models;
-using System.Linq;
 
-public class LoginModel : PageModel
+namespace SwiftPizza.Views.Home
 {
-    private readonly ILogger<LoginModel> _logger;
-    private readonly ApplicationDbContext _context;
-    private readonly IConfiguration _configuration;
-
-    [BindProperty]
-    public string Email { get; set; }
-
-    [BindProperty]
-    public string Password { get; set; }
-
-    public LoginModel(ILogger<LoginModel> logger, ApplicationDbContext context, IConfiguration configuration)
+    public class LoginModel : PageModel
     {
-        _logger = logger;
-        _context = context;
-        _configuration = configuration;
-    }
-
-    public IActionResult OnGet()
-    {
-        return Page();
-    }
-
-    public IActionResult OnPost()
-    {
-        // Retrieve user from the database based on the provided email
-        var user = _context.Users.FirstOrDefault(u => u.Email == Email);
-
-        if (user != null && user.Password == Password)
+        public void OnGet()
         {
-            // Authentication successful, you can store user information in a session or cookie
-            // For simplicity, this example just redirects to a protected page
-            return RedirectToPage("/ProtectedPage");
         }
-
-        // Authentication failed
-        ModelState.AddModelError(string.Empty, "Invalid login attempt.");
-        return Page();
     }
-
 }
